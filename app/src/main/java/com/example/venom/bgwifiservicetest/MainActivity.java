b760f0a3c8e282cd87801249be35a6c8f140c266
Preston <cpreston@pdx.edu>
2018-03-02 11:07:33 -0800

    added device info textview and telephonymanger to get phone number

--- app/src/main/java/com/example/venom/bgwifiservicetest/MainActivity.java
+++ app/src/main/java/com/example/venom/bgwifiservicetest/MainActivity.java
@@ -1,17 +1,25 @@
 package com.example.venom.bgwifiservicetest;
 
+import android.Manifest;
+import android.accessibilityservice.AccessibilityService;
 import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     BackgroundWifiService mBGWService;
     Intent mServiceIntent;
     Context ctx;
+
     public Context getCtx() {
         return ctx;
     }
@@ -25,6 +33,44 @@ public class MainActivity extends AppCompatActivity {
         mServiceIntent = new Intent(getCtx(), mBGWService.getClass());
         if (!isMyServiceRunning(mBGWService.getClass())) {
             startService(mServiceIntent);
+        }
+
+        final TextView textView = (TextView) findViewById(R.id.main_text);
+        textView.setText(
+                "SERIAL: " + Build.SERIAL + "\n" +
+                        "MODEL: " + Build.MODEL + "\n" +
+                        "ID: " + Build.ID + "\n" +
+                        "Manufacture: " + Build.MANUFACTURER + "\n" +
+                        "Brand: " + Build.BRAND + "\n" +
+                        "Type: " + Build.TYPE + "\n" +
+                        "User: " + Build.USER + "\n" +
+                        "BASE: " + Build.VERSION_CODES.BASE + "\n" +
+                        "INCREMENTAL: " + Build.VERSION.INCREMENTAL + "\n" +
+                        "SDK:  " + Build.VERSION.SDK + "\n" +
+                        "BOARD: " + Build.BOARD + "\n" +
+                        "BRAND: " + Build.BRAND + "\n" +
+                        "HOST: " + Build.HOST + "\n" +
+                        "FINGERPRINT: "+Build.FINGERPRINT + "\n" +
+                        "Version Code: " + Build.VERSION.RELEASE);
+
+        TelephonyManager tMgr = (TelephonyManager) ctx.getSystemService(Context.TELEPHONY_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        String mPhoneNumber = tMgr.getLine1Number();
+        textView.setText(textView.getText() + "\n" + mPhoneNumber);
+
+        if (mPhoneNumber == null || mPhoneNumber.equals("") || mPhoneNumber.equals("???????")) {
+            mPhoneNumber = tMgr.getDeviceId();
+            textView.setText(textView.getText() + "\n" + mPhoneNumber);
         }
     }
 
