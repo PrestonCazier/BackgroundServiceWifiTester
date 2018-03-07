5acbf84e3c1ac4313169b05447b34b2fae06d991
Preston <cpreston@pdx.edu>
2018-03-07 08:45:35 -0800

    added device info textview and telephonymanger to get phone number

---
+++ app/src/main/java/com/example/venom/bgwifiservicetest/AlarmServer.java
@@ -0,0 +1,12 @@
+package com.example.venom.bgwifiservicetest;
+
+import android.app.AlarmManager;
+
+/**
+ * Created by Venom on 3/7/2018.
+ */
+
+public class AlarmServer extends AlarmManager {
+    AlarmServer() {
+    }
+}
